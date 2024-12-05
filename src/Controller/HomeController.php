6d1f5c6279a46e22a0c1b9a2df0c4bce7304d613
Noe DAMBREVILLE <n.dambreville@cite-scolaire.lan>
2024-12-05 10:12:31 +0100

    debut de la configuration du Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    // #[Route('/', name: 'app_home', methode[GET] )] exemple
+    #[Route('/', name: 'app_home' )]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
