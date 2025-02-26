eb430d7c709c5b12a45545f9d61c6ed87afad691
🖐 <147812434+dnoe-pro@users.noreply.github.com>
2025-02-26 11:33:16 +0100

    save

--- public/js/curseur-gradiant.js
+++ public/js/curseur-gradiant.js
@@ -1,40 +1,32 @@
-// Fonction pour initialiser le curseur
 const initializeCircleCursor = () => {
   const circle = document.getElementById("cursor-circle");
   if (!circle) return;
   let mouseX = 0, mouseY = 0;
 
-  const updatePosition = () => {   // Fonction de mise à jour de la position
+  const updatePosition = () => {
       circle.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
       requestAnimationFrame(updatePosition);
   };
 
-  // Suivi de la position du curseur
   document.addEventListener('mousemove', (e) => {
       const offsetX = circle.offsetWidth / 2;
       const offsetY = circle.offsetHeight / 2;
       mouseX = e.clientX - offsetX;
       mouseY = e.clientY - offsetY;
   });
-
-  // Lance la boucle d'animation
   updatePosition();
 };
 
-// Fonction pour initialiser les événements de la page
 const init = () => {
   initializeCircleCursor();
   setupAJAXNavigation();
 };
 
-// Configuration de la navigation AJAX pour charger les pages sans rechargement complet
 const setupAJAXNavigation = () => {
   const links = document.querySelectorAll("a");
-
   links.forEach(link => {
       link.addEventListener("click", async (e) => {
           if (link.target === "_blank" || link.href.startsWith("#")) return;
-
           e.preventDefault();
           const response = await fetch(link.href, { headers: { "X-Requested-With": "XMLHttpRequest" } });
           if (!response.ok) {
@@ -45,13 +37,10 @@ const setupAJAXNavigation = () => {
           const html = await response.text();
           const parser = new DOMParser();
           const doc = parser.parseFromString(html, "text/html");
-
-          // Remplace le contenu principal
           const newContent = doc.querySelector("body");
           document.body.innerHTML = newContent.innerHTML;
-          init();// Réinitialise les scripts
+          init();
       });
   });
 };
-// Exécute l'initialisation des événements au chargement du document
 document.addEventListener("DOMContentLoaded", init);
---
+++ public/js/tailwindconfig.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                // theme blanc
+                cSection: "#f7fafd",
+
+                //theme foncé
+                dSection: "#26292F",
+            }
+        }
+    }
+}
