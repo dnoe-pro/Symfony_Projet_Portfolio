06c950fabb59fac3ed9e7b9afe481dd6105efdb1
🖐 <147812434+dnoe-pro@users.noreply.github.com>
2025-03-07 19:01:19 +0100

    ajout des palettes de couleur

--- public/js/theme/tailwindconfig.js
+++ public/js/theme/tailwindconfig.js
@@ -3,19 +3,29 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
+                // couleurs de base //
+                // " #292521",
+                // " #7E8EE6",
+                // " #F5EEE4",
+
+                // " #24272c",
+                // " #2e3139",
+
+
+
                 //theme sombre
                 dSection: " #24272c",
                 dBackground: " #2e3139",
 
                 // theme clair
-                cSection: " #f7fafd",
-                cBackground: " #d8e7f5",
+                cSection: " #EBEBE8",
+                cBackground: " #d7d7d5",
+
+                // 2 themes
+                dText:" #4172ee",
+                fText:" #6087fe",
 
             }
         }
     }
 }
-
-
-
-
