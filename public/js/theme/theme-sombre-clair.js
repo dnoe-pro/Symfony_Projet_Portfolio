6f0fd28bf12b94526916653610a683f74b054bf5
🖐 <147812434+dnoe-pro@users.noreply.github.com>
2025-03-07 19:01:53 +0100

    Amélioration du code

--- public/js/theme/theme-sombre-clair.js
+++ public/js/theme/theme-sombre-clair.js
@@ -1,19 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggle = document.getElementById('theme-toggle');
     const html = document.documentElement;
-    if (localStorage.getItem('theme') === 'dark') {
+    let isDarkMode = localStorage.getItem('theme') === 'dark';
+
+    if (isDarkMode) {
         html.classList.add('dark');
     }
+    let animation = lottie.loadAnimation({
+        container: document.getElementById('lottie-container'),
+        renderer: 'svg',
+        loop: false, 
+        autoplay: false, 
+        path: '../json/clair-sombre.json' 
+    });
+    animation.addEventListener('DOMLoaded', function () {
+        animation.goToAndStop(isDarkMode ? 14 : 0, true); // 14 : mode sombre, 0 : mode clair
+    });
     toggle.addEventListener('click', function () {
-        if (html.classList.contains('dark')) {
-            html.classList.remove('dark');
-            localStorage.setItem('theme', 'light');
+        isDarkMode = !isDarkMode;
+        if (isDarkMode) {
+            animation.playSegments([0, 14], true); 
+            html.classList.add('dark'); 
+            localStorage.setItem('theme', 'dark'); 
         } else {
-            html.classList.add('dark');
-            localStorage.setItem('theme', 'dark');
+            animation.playSegments([14, 0], true);
+            html.classList.remove('dark'); 
+            localStorage.setItem('theme', 'light');
         }
     });
+    animation.addEventListener('complete', function () {
+        animation.goToAndStop(isDarkMode ? 14 : 0, true);
+    });
 });
-
-
-
